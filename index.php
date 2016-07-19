2f1c5f207dbf2cc0ec65e8cd62c4e5ffcb89a779
BraianCoronel <liveefull@gmail.com>
2016-07-19 18:03:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Usar los servidores de Google sería la gloria.<\br><\br> Ellos se encargan de la configuración de 
+echo "Usar los servidores de Google sería la gloria.<br><br> Ellos se encargan de la configuración de 
 hardware en caso de que la aplicación empiece a escalar";
 
 ?>
